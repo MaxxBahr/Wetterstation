4b51278f60ec5d5a0df072e26a832496ce7e5d3c
MaxxBahr <schnee.bahr@googlemail.com>
2023-11-16 20:41:27 +0100

    added test file for template rendering

--- web/app.js
+++ web/app.js
@@ -6,6 +6,8 @@ mongoose.set("strictQuery", false);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "\\views");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -23,9 +25,22 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.get("/test", (req, res) =>{
+  try{
+    res.status(200);
+    const data = {
+      title: "Hallo",
+      message: "Wie gehts dir Louai?"
+    };
+    res.render("index", data);
+  }catch (e){
+    console.log(e.message);
+  }
+})
+
 const start = async () => {
   try {
-    await mongoose.connect(CONNECTION);
+    //await mongoose.connect(CONNECTION);
 
     app.listen(PORT, () => {
       console.log("Server running on Port:" + PORT);
